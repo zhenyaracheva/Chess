c9f3bab337037ce7f37092801db077b3ba9c38ae
zhenyaracheva <zhenia.racheva@gmail.com>
2015-08-18 17:22:22 +0300

    Added Bishop movement

--- Chess/Chess/Movements/BishopMovement.cs
+++ Chess/Chess/Movements/BishopMovement.cs
@@ -8,19 +8,47 @@
 
     public class BishopMovement : IMovement
     {
+        private int rowDirection;
+        private int colDirection;
+
         public void ValidateMove(IFigure figure, IBoard board, Helpers.Move move)
         {
             var rowDistance = Math.Abs(move.From.Row - move.To.Row);
             var colDistance = Math.Abs(move.From.Col - move.To.Col);
-
 
             if (rowDistance != colDistance)
             {
                 throw new InvalidOperationException("Invalid Bishop move!");
             }
 
-            // TODO: extract to method
-            var other = figure.Color == FigureColor.White ? FigureColor.Black : FigureColor.White;
+            var otherColor = figure.Color == FigureColor.White ? FigureColor.Black : FigureColor.White;
+
+            rowDirection = move.From.Row > move.To.Row ? -1 : 1;
+            colDirection = move.From.Col > move.To.Col ? -1 : 1;
+
+            var row = move.From.Row;
+            var col = move.From.Col;
+
+            var endRow = move.To.Row + rowDirection * (-1);
+            var endCol = move.To.Col + colDirection * (-1);
+
+            while (row != endRow && col != endCol)
+            {
+                row += rowDirection;
+                col += colDirection;
+
+                if (board.SeeFigureOnPosition(row, col) != null)
+                {
+                    throw new InvalidOperationException("Invalid Bishop move!");
+                }
+            }
+
+            var toPositionFigure = board.SeeFigureOnPosition(move.To.Row, move.To.Col);
+
+            if (toPositionFigure != null && toPositionFigure.Color == figure.Color)
+            {
+                throw new InvalidOperationException("Position is already taken!");
+            }
         }
     }
 }
